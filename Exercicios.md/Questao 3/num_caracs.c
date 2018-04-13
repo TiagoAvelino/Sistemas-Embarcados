8e81b91fee5b71528ee605cae8582451da7e2df1
TiagoAvelino <30874130+TiagoAvelino@users.noreply.github.com>
2018-04-13 11:58:09 -0300

    Add files via upload

---
+++ Exercicios.md/Questao 3/num_caracs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "num_caracs.h"
+int Num_Caracs(char *string){
+int i=0;
+
+for(i=0; string [i] != '\0';i++){
+}
+return (i-1);
+}
+
+int main (int argc, char *argv[]){
+
+
+}
