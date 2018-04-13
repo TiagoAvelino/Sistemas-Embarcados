6d09f1004a97156128ba5eabf4b680bc6bd2fac9
TiagoAvelino <30874130+TiagoAvelino@users.noreply.github.com>
2018-04-13 15:58:32 -0300

    Add files via upload

---
+++ Exercicios.md/Questao_4/Questao2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	char Nome[20],NomeArq[20];
+	int idade=0;
+   
+	FILE *fp;
+
+	printf("Digite seu nome:");
+
+	scanf("%s",Nome); 	
+
+	printf("\nDigite sua idade:");
+
+	scanf(" %d ", idade);
+
+while (Nome[i] != "\0"){
+	NomeArq[i]= Nome[i];
+	i++;
+	}
+
+	strcat(NomeArq. ".txt");
+	fopen( )
+}
